9821fe034aec8082c2ecbcca659c022405cdefb8
JeromeHux <jerome@jeudi.beer>
2020-06-20 15:19:49 +0200

    return unmodifiableList

--- src/main/java/domain/History.java
+++ src/main/java/domain/History.java
@@ -3,14 +3,16 @@ package domain;
 import java.util.LinkedList;
 import java.util.List;
 
+import static java.util.Collections.unmodifiableList;
+
 class History {
     private List<HistoryLine> historyLines = new LinkedList<>();
 
     void addHistoryLine(Operation operation, Amount balance) {
-        historyLines.add(0,new HistoryLine(operation, balance));
+        historyLines.add(0, new HistoryLine(operation, balance));
     }
 
     List<HistoryLine> historyLines() {
-        return historyLines;
+        return unmodifiableList(historyLines);
     }
 }
